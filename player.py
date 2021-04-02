38fba8ae3c082f6e6c023c61ea901f45747ac39e
Amina BENDJEBBOUR <bendjebbour.amina@yahoo.fr>
2021-04-02 16:25:42 +0200

    modif code player

--- player.py
+++ player.py
@@ -1,11 +1,11 @@
- class Player:
+class Player:
 
-def __init__(self,nom):
-    self.__nom=nom
+    def __init__(self,nom):
+        self.__nom=nom
           
     def get_nom(self): #[Encapsulation] 
         return self.__nom
   
     def __str__(self):
          
-        return f"Le nom de joueur est: {self.__nom} "
+        return f"Le nom de joueur est: {self.__nom}."

64de0a97c71c26bc23e316158073c5f5a3aaa610
Amina BENDJEBBOUR <bendjebbour.amina@yahoo.fr>
2021-04-02 16:22:33 +0200

    ajjouter les fichier d'execution et commencer le code de player

--- player.py
+++ player.py
@@ -1,1 +1,11 @@
- 
+ class Player:
+
+def __init__(self,nom):
+    self.__nom=nom
+          
+    def get_nom(self): #[Encapsulation] 
+        return self.__nom
+  
+    def __str__(self):
+         
+        return f"Le nom de joueur est: {self.__nom} "

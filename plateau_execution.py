ebd9a02e9e7922ab7a1dcf06a54d5d197b44f013
Amina BENDJEBBOUR <bendjebbour.amina@yahoo.fr>
2021-04-02 16:29:49 +0200

    modif

---
+++ plateau_execution.py
@@ -0,0 +1,6 @@
+if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
+    # Votre programme principal ne sera pas évalué.
+    # Utilisez-le pour tester votre programme en faisant
+    # les appels de votre choix.
+    # Respectez bien ce niveau d'identation.
+    print("Debut du prog. principal")
